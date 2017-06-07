b8b07682050f96d9f67e12b1f7700c351e365f0d
jcostopulos <costopulos@gmail.com>
2017-06-07 12:28:04 -0700

    Firebase Funtional

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,108 @@
-// Pseudocode
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBjq2Vj8MkTlkdUkIh7DNZx8aXxXi8ATkg",
+  authDomain: "train-scheduler-d8c5c.firebaseapp.com",
+  databaseURL: "https://train-scheduler-d8c5c.firebaseio.com",
+  projectId: "train-scheduler-d8c5c",
+  storageBucket: "train-scheduler-d8c5c.appspot.com",
+  messagingSenderId: "765894655054"
+};
+
+firebase.initializeApp(config);
+                    // GLOBAL VARIABLES. 
+//----------------------------------------------------------------------------------
+var database = firebase.database();
+
+var nameOfTrain, whereTo, firstTrainTime, freq;
 
 
-// create variables to capture user input.
+//----------------------------------------------------------------------------------
+database.ref().on("value", function(snapshot) { // This function is the setting function from Firebase.
 
+console.log(snapshot.val());
+
+nameOfTrain = snapshot.val().train;
+whereTo = snapshot.val().destination;
+firstTrainTime = snapshot.val().departure;
+freq = snapshot.val().frequency;
+
+//Dynamically inserts table rows with concatenated variables.
+var tableRow =
+$("<tr><td>" + nameOfTrain
++ "</td><td>" + whereTo
++ "</td><td>" + freq
++ "</td><td>" + // calculation function (variable) for NEXT ARRIVAL field goes here.
++ "</td><td>" + // calculation function (variable) for MINUTES AWAY field goes here.
++ "</td></tr>");
+
+// appends tablename (user data) into trainTable table on DOM.
+$("#trainTable").append(tableRow);
+
+}); //Setting function endtag
+
+//----------------------------------------------------------------------------------
+
+// onclick for submit buttons.
 $("#formSubmission").on("click", function(event){
   event.preventDefault();
 
-var nameOfTrain = $("#trainName").val().trim();
-var WhereTo = $("#destination").val().trim();
-var trainOne = $("#firstTrain").val().trim();
-var freq = $("#frequency").val().trim();
+//Pulls user input data from fields.
+nameOfTrain =    $("#trainName").val().trim();
+whereTo =        $("#destination").val().trim();
+firstTrainTime = $("#firstTrain").val().trim();
+freq =           $("#frequency").val().trim();
 
-console.log(nameOfTrain);
-console.log(WhereTo);
-console.log(trainOne);
-console.log(freq);
+                //Math manipulation of First Train Arrival user input.
+//----------------------------------------------------------------------------------
+var firstHourSliced = firstTrainTime.slice(0, 2); //returns only hours of time input
+var firstHourParsed = parseInt(firstHourSliced, 10);
 
-});
+var firstMinutesSliced = firstTrainTime.slice(3, 5); //returns only minutes of time input
+var firstMinutesParsed = parseInt(firstMinutesSliced, 10);
+
+var x = ((firstHourParsed * 60) + firstMinutesParsed);
+// console.log("this is x " + x);
+//----------------------------------------------------------------------------------
+
+              //Math manipulation of Frequency user input.
+//----------------------------------------------------------------------------------
+var freqHoursSliced = freq.slice(0, 2);
+var freqHoursParsed = parseInt(freqHoursSliced, 10);
+// console.log("this is freqHoursParsed " + freqHoursParsed);
+
+var freqMinutesSliced = freq.slice(3, 5);
+var freqMinutesParsed = parseInt(freqMinutesSliced, 10);
+// console.log("this is freqMinutesParsed " + freqMinutesParsed);
+
+var y = ((freqHoursParsed * 60) + freqMinutesParsed);
+// console.log("this is y " + y);
+//----------------------------------------------------------------------------------
+
+var calculated = x + y
+console.log("this is the first arrival + frequency in minutes " + calculated);
+
+// function calcTime (){
+//   // var timeResult =
+// };
+
+//Sets user data to Firebase.
+database.ref().set({
+  train: nameOfTrain,
+  destination: whereTo,
+  departure: firstTrainTime,
+  frequency: freq
+})
+
+
+
+// clears user input fields.
+$("#trainName").val(' ');
+$("#destination").val(' ');
+// $("#firstTrain").val(' ');
+// $("#frequency").val(' ');
+//----------------------------------------------------------------------------------
+
+
+
+}); //Onclick function endtag
